f62814bb717b3f12f2c47b0ba5b3d4f96d3628fe
Abhinay Pandey <abhinaypandey02@gmail.com>
2024-02-01 12:30:55 +0530

    Add typescript support for passing arguments to registerApolloClient
    Added ...args: any[] for typescript to accept any arguments being passed onto the registerApolloClient function to have dynamic cached apollo client based on the arguments

--- package/src/rsc/registerApolloClient.tsx
+++ package/src/rsc/registerApolloClient.tsx
@@ -3,7 +3,7 @@ import "server-only";
 import type { ApolloClient } from "@apollo/client";
 import * as React from "react";
 
-export function registerApolloClient(makeClient: () => ApolloClient<any>) {
+export function registerApolloClient(makeClient: (...args: any[]) => ApolloClient<any>) {
   const getClient = React.cache(makeClient);
   return {
     getClient,
